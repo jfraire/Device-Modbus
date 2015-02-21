bb69c36cf0d04c4870b7f87704fa33528f669a60
Julio Fraire <julio.fraire@gmail.com>
2015-02-21 18:51:58 +0100

    Simplified example spy

--- examples/modbus_spy.pl
+++ examples/modbus_spy.pl
@@ -8,16 +8,12 @@ say "Starting Modbus Spy";
 
 my $spy = Device::Modbus::Spy->new(
     port     => '/dev/ttyUSB0',
-    baudrate => 19200,
+    baudrate => 9600,
     parity   => 'none',
 );
 
 while (1) {
-    $spy->watch_port;
-
-    say $spy->message;
-    say $spy->unit;
-    say $spy->pdu;
-    say $spy->cdc;
+    say $spy->watch_port;
+    say $spy->object if defined $spy->object;
     say '---';
 }
